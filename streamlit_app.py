d735f438049b673e33eb0750d000b227ae569d54
André RIcardo <andreralves@hotmail.com>
2024-08-09 01:05:18 -0300

    docs e custom pages
    customização e comentários

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,54 +2,66 @@ import streamlit as st
 from typing import Generator
 from groq import Groq
 
-st.set_page_config(page_icon="💬", layout="wide",
-                   page_title="Groq Goes Brrrrrrrr...")
+st.set_page_config(page_icon="🕵️‍♂️", layout="wide",
+                   page_title="AndreIa - chat")
 
 
 def icon(emoji: str):
-    """Shows an emoji as a Notion-style page icon."""
+    """AndreIa"""
     st.write(
-        f'<span style="font-size: 78px; line-height: 1">{emoji}</span>',
+        f'<span style="font-size: 30px; line-height: 1">{emoji}</span>',
         unsafe_allow_html=True,
     )
 
 
-icon("🏎️")
+icon("🕵️‍♂️")
 
-st.subheader("Groq Chat Streamlit App", divider="rainbow", anchor=False)
+st.subheader(":green[AndreIa] - Assistente I.A. do André", divider="green", anchor=False)
+textArea='''
+  **T A R E F A S ✔️**
 
+  * [  ] Tarefa 1 - Texto descrevendo casos de uso de cada modelo  [U]
+  * [  ] Tarefa 2 - componente copiar texto da última mensagem
+  * [  ] Tarefa 2 - Dockerizar o app
+  * [  ] Tarefa 3 - Especializar novos agentes (T.I, Saúde, Música, Cinema)
+  * [  ] Tarefa 4 - 
+  * [  ] Tarefa 5 - 
+'''
+st.markdown(textArea)
 client = Groq(
     api_key=st.secrets["GROQ_API_KEY"],
 )
 
-# Initialize chat history and selected model
+# Inicializa o histórico de acordo com o modelo selecionado. 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 if "selected_model" not in st.session_state:
     st.session_state.selected_model = None
 
-# Define model details
+# Define detalhes dos modelos
 models = {
-    "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"},
-    "llama2-70b-4096": {"name": "LLaMA2-70b-chat", "tokens": 4096, "developer": "Meta"},
+    "gemma-7b-it": {"name": "Gemma-7b-it", "tokens": 8192, "developer": "Google"},                          
+    "gemma2-9b-it": {"name": "gemma2-9b-it", "tokens": 8192, "developer": "Google"}, #new
+    "llama-3.1-70b-versatile": {"name": "llama-3.1-70b-versatile", "tokens": 8000, "developer": "Meta"}, #new
+    # "llama2-70b-4096": {"name": "LLaMA2-70b-chat", "tokens": 4096, "developer": "Meta"},
     "llama3-70b-8192": {"name": "LLaMA3-70b-8192", "tokens": 8192, "developer": "Meta"},
     "llama3-8b-8192": {"name": "LLaMA3-8b-8192", "tokens": 8192, "developer": "Meta"},
     "mixtral-8x7b-32768": {"name": "Mixtral-8x7b-Instruct-v0.1", "tokens": 32768, "developer": "Mistral"},
 }
 
-# Layout for model selection and max_tokens slider
+# Layout para o modelo selecionado e o slider max_tokens
 col1, col2 = st.columns(2)
 
 with col1:
     model_option = st.selectbox(
-        "Choose a model:",
+        "Escolha um modelo, de acordo com sua necessidade:",
         options=list(models.keys()),
         format_func=lambda x: models[x]["name"],
-        index=4  # Default to mixtral
+        index=4  # Default : mixtral
     )
 
-# Detect model change and clear chat history if model has changed
+# Detecta o modelo modificado e limpa o histórico do chat ao modificar o modelo.
 if st.session_state.selected_model != model_option:
     st.session_state.messages = []
     st.session_state.selected_model = model_option
@@ -57,18 +69,18 @@ if st.session_state.selected_model != model_option:
 max_tokens_range = models[model_option]["tokens"]
 
 with col2:
-    # Adjust max_tokens slider dynamically based on the selected model
+    # Ajusta max_tokens slider dinamicamente, de acordo com o modelo selecionado
     max_tokens = st.slider(
         "Max Tokens:",
-        min_value=512,  # Minimum value to allow some flexibility
+        min_value=512,  # Valor mínimo para ter alguma flexibilidade
         max_value=max_tokens_range,
-        # Default value or max allowed if less
+        # Valor padrão ou o máximo de tokens do modelo se vazio.
         value=min(32768, max_tokens_range),
         step=512,
-        help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}"
+        help=f"Ajuste o número máximo de tokens (palavras) para a resposta do modelo. Máximo para o modelo selecionado: {max_tokens_range}"
     )
 
-# Display chat messages from history on app rerun
+# Mostra as mensagens de chat do histórico ao subir a aplicação 
 for message in st.session_state.messages:
     avatar = '🤖' if message["role"] == "assistant" else '👨‍💻'
     with st.chat_message(message["role"], avatar=avatar):
@@ -76,13 +88,13 @@ for message in st.session_state.messages:
 
 
 def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
-    """Yield chat response content from the Groq API response."""
+    """Gerar o conteúdo da resposta do chat a partir da resposta da API Groq."""
     for chunk in chat_completion:
         if chunk.choices[0].delta.content:
             yield chunk.choices[0].delta.content
 
 
-if prompt := st.chat_input("Enter your prompt here..."):
+if prompt := st.chat_input("Digite seu prompt aqui..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     with st.chat_message("user", avatar='👨‍💻'):
